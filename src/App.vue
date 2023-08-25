450e8a59532a25f8fdf629dc08e23d46d23a6e51
Lina <lsabogalarias@gmail.com>
2023-08-24 22:51:45 -0500

    nueva componente

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MovieCard v-for="(movie, index) in movies" :key="index" :title="movie.title" :image="movie.image" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MovieCard from "./components/MovieCard.vue";
 
 export default {
   name: 'App',
+  data(){
+    return{
+      movies: [
+        {title: 'El señor de los anillos', image: 'https://es.web.img3.acsta.net/medias/nmedia/18/89/67/45/20061512.jpg'},
+        {title: 'Interestelar', image: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/79194981293eabf6620ece96eb5a9c1fffa04d3374ae12986e0748800b37b9cf._RI_TTW_.jpg'},
+        {title: 'Avatar', image: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/ae4816cade1a5b7f29787d0b89610132c72c7747041481c6619b9cc3302c0101._RI_TTW_.jpg'},
+        {title: 'Gladiador', image: 'https://es.web.img3.acsta.net/medias/nmedia/18/70/92/02/20149073.jpg'}
+      ]
+    }      
+  }, 
   components: {
-    HelloWorld
-  }
-}
+    MovieCard   
+  }  
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable-next-line no-return-assign, no-param-reassign -->
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <img width="100" v-bind:src="image" v-bind:alt="title"/>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: ['image', 'title'],
+};
+</script>
